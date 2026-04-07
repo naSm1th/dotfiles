d73c8df01731c90d6cab7a81de779e99e87b6579
Nathanael Smith <naSm1th@users.noreply.github.com>
2026-04-06 23:16:09 -0500

    Get whisper and piper setup

--- hosts/fiona/configuration.nix
+++ hosts/fiona/configuration.nix
@@ -68,6 +68,23 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.wyoming = {
+    faster-whisper.servers.ha = {
+      enable = true;
+      device = "cpu";
+      model = "tiny-int8";
+      uri = "tcp://localhost:10300";
+      language = "en";
+    };
+    piper.servers.ha = {
+      enable = true;
+      voice = "en_GB-semaine-medium";
+      speaker = 0;
+      uri = "tcp://localhost:10200";
+      streaming = true;
+    };
+  };
+
   virtualisation = {
     containers.enable = true;
     podman = {
@@ -77,23 +94,25 @@
     };
     oci-containers = {
       backend = "podman";
-      containers.homeassistant = {
-        volumes = [
-          "/var/lib/homeassistant:/config"
-          "/run/dbus:/run/dbus:ro"
-        ];
-        environment.TZ = "America/Chicago";
-        # Note: The image will not be updated on rebuilds, unless the version label changes
-        image = "ghcr.io/home-assistant/home-assistant:2026.4.1";
-        extraOptions = [ 
-          # Use the host network namespace for all sockets
-          "--network=host"
-          # Permit bluetooth
-          "--cap-add=NET_ADMIN"
-          "--cap-add=NET_RAW"
-          # Pass devices into the container, so Home Assistant can discover and make use of them
-          # "--device=/dev/ttyACM0:/dev/ttyACM0"
-        ];
+      containers = {
+        homeassistant = {
+          volumes = [
+            "/var/lib/homeassistant:/config"
+            "/run/dbus:/run/dbus:ro"
+          ];
+          environment.TZ = "America/Chicago";
+          # Note: The image will not be updated on rebuilds, unless the version label changes
+          image = "ghcr.io/home-assistant/home-assistant:2026.4.1";
+          extraOptions = [ 
+            # Use the host network namespace for all sockets
+            "--network=host"
+            # Permit bluetooth
+            "--cap-add=NET_ADMIN"
+            "--cap-add=NET_RAW"
+            # Pass devices into the container, so Home Assistant can discover and make use of them
+            # "--device=/dev/ttyACM0:/dev/ttyACM0"
+          ];
+        };
       };
     };
   };
